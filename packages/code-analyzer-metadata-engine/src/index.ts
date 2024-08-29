d907c2d580b5112d5e281c1b792b4b1c4aba76d0
Stephen Carter <123964848+stephen-carter-at-sf@users.noreply.github.com>
2024-08-29 10:54:34 -0400

    CHANGE: @W-16605635@ Update package versions and remove metadata engi… (#81)

--- packages/code-analyzer-metadata-engine/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { MetadataEnginePlugin } from "./plugin";
-import {EnginePlugin} from "@salesforce/code-analyzer-engine-api";
-
-function createEnginePlugin(): EnginePlugin {
-    return new MetadataEnginePlugin();
-}
-
-export { createEnginePlugin, MetadataEnginePlugin }
