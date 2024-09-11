3fc33705593c937ac44c68ff9f9a6901047c82df
Joshua Feingold <jfeingold35@gmail.com>
2024-09-11 14:00:19 -0500

    temporary commit

--- packages/code-analyzer-flowtest-engine/test/python/FlowTestWrapper.test.ts
+++ packages/code-analyzer-flowtest-engine/test/python/FlowTestWrapper.test.ts
@@ -1,4 +1,5 @@
 import {FlowTestWrapper} from '../../src/python/FlowTestWrapper';
+import which from 'which';
 
 describe('FlowTestWrapper', () => {
 
@@ -6,10 +7,12 @@ describe('FlowTestWrapper', () => {
     describe('runFlowTestHelpCommand', () => {
 
         it('Works', async () => {
-            const wrapper = new FlowTestWrapper('/Library/Frameworks/Python.framework/Versions/3.12/bin/python3');
+            const python3 = await which('python3');
+            const wrapper = new FlowTestWrapper(python3);
+            //const wrapper = new FlowTestWrapper('/Library/Frameworks/Python.framework/Versions/3.12/bin/python3');
 
-            const s: string = await wrapper.wherePython();
-            const s2 = await wrapper.runPythonVersion();
+            const s: string = await wrapper.wherePython(); // which python3 => /Library/Frameworks/Python.framework/Versions/3.12/bin/python3
+            const s2 = await wrapper.runPythonVersion(); // python3 --version => 3.9.6
             const s3 = await wrapper.runPythonInfoScript();
             const s4 = await wrapper.runFlowTestHelpCommand();
 
@@ -19,7 +22,7 @@ describe('FlowTestWrapper', () => {
             console.log(s4);
             expect(s).toContain('3.12');
             expect(s2).toContain('Python 3.12.4');
-            expect(s4).toContain('usage: flowtest');
+            expect(s4).toContain('usage: __main__');
         });
     });
 });
