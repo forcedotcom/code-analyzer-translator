466022a4bb0cb032566c28030d0280e21c75fc54
Joshua Feingold <jfeingold35@gmail.com>
2024-09-26 14:37:36 -0500

    NEW @W-16360876@ Feedback from code review.

--- packages/code-analyzer-flowtest-engine/src/python/FlowTestCommandWrapper.ts
+++ packages/code-analyzer-flowtest-engine/src/python/FlowTestCommandWrapper.ts
@@ -6,13 +6,37 @@ export interface FlowTestCommandWrapper {
     getFlowTestRuleDescriptions(): Promise<FlowTestRuleDescriptor[]>;
 }
 
+/**
+ * The format used by FlowTest to describe the Queries (FlowTest's term for what we'd call rules) it can run.
+ */
 export type FlowTestRuleDescriptor = {
+    /**
+     * A machine-readable ID consisting of a unique string. E.g., "FlowSecurity.SystemModeWithSharing.recordCreates.data".
+     */
     query_id: string;
+    /**
+     * A human-readable name consisting of a unique string. E.g., "Flow: SystemModeWithSharing recordCreates data".
+     */
     query_name: string;
+    /**
+     * A string indicating relative severity. either {@code Flow_Low_Severity}, {@code Flow_Moderate_Severity}, or {@code Flow_High_Severity}.
+     */
     severity: string;
+    /**
+     * A string describing the specific case the query identifies.
+     */
     query_description: string;
+    /**
+     * A URL for help text.
+     */
     help_url: string;
+    /**
+     * Not a property we care about.
+     */
     query_version: string;
+    /**
+     * A string-wrapped boolean indicating whether the rule is security-related.
+     */
     is_security: string;
 }
 
