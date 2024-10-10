013083d2b114a99723468b3422036dd008dde020
Joshua Feingold <jfeingold35@gmail.com>
2024-10-10 11:06:57 -0500

    NEW @W-16360921@ Fixed flapping test

--- packages/code-analyzer-flowtest-engine/test/test-data/executable-scripts/error-thrower.py
+++ packages/code-analyzer-flowtest-engine/test/test-data/executable-scripts/error-thrower.py
@@ -4,7 +4,6 @@ import sys
 print('Logging the number "1" to stderr', file=sys.stderr)
 print('Logging the number "2" to stderr', file=sys.stderr)
 print('Logging the number "87" to stdout')
-# Print an empty string to stderr
-print('', file=sys.stderr)
+print('Logging a string\nwith newline characters\n\nin it\nto stderr', file=sys.stderr)
 print('Logging the string "orangutan" to stderr', file=sys.stderr)
 sys.exit(42)
