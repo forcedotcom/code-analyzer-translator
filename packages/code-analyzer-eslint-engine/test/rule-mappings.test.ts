9b81fbcbe312262135bd3d71c1eca577d82c0620
Stephen Carter <123964848+stephen-carter-at-sf@users.noreply.github.com>
2024-12-20 16:49:42 -0500

    NEW(eslint): @W-17378695@: Add in rule mappings to lock in severities and tags for eslint (#179)

--- packages/code-analyzer-eslint-engine/test/rule-mappings.test.ts
+++ packages/code-analyzer-eslint-engine/test/rule-mappings.test.ts
@@ -1,25 +1,25 @@
-import {PmdEngine} from "../src/pmd-engine";
-import {DEFAULT_PMD_ENGINE_CONFIG} from "../src/config";
 import {RuleDescription} from "@salesforce/code-analyzer-engine-api";
-import {RULE_MAPPINGS} from "../src/pmd-rule-mappings";
+import {RULE_MAPPINGS} from "../src/rule-mappings";
+import {ESLintEngine} from "../src/engine";
+import {DEFAULT_CONFIG} from "../src/config";
 
 describe('Tests for the rule-mappings', () => {
-    it('Test that the list of all bundled PMD rules (from all languages) matches the list of languages in our RULE_MAPPINGS list', async () => {
-        const engine: PmdEngine = new PmdEngine(DEFAULT_PMD_ENGINE_CONFIG);
+    it('Test that the list of all bundled rules matches our RULE_MAPPINGS list', async () => {
+        const engine: ESLintEngine = new ESLintEngine(DEFAULT_CONFIG);
         const ruleDescriptions: RuleDescription[] = await engine.describeRules({});
         const actualRuleNames: Set<string> = new Set(ruleDescriptions.map(rd => rd.name));
         const ruleNamesInRuleMappings: Set<string> = new Set(Object.keys(RULE_MAPPINGS));
 
         const unusedRuleNamesFromRuleMappings: Set<string> = setDiff(ruleNamesInRuleMappings, actualRuleNames);
         if (unusedRuleNamesFromRuleMappings.size > 0) {
-            throw new Error("The following rule names are found in RULE_MAPPINGS but do not associated to any bundled PMD rules (maybe PMD deprecated the rule):\n    " +
+            throw new Error("The following rule names are found in RULE_MAPPINGS but do not associated to any bundled eslint rules that come with our base configs:\n    " +
                 [...unusedRuleNamesFromRuleMappings].join(', '));
 
         }
 
         const missingRuleNamesFromRuleMappings: Set<string> = setDiff(actualRuleNames, ruleNamesInRuleMappings);
         if (missingRuleNamesFromRuleMappings.size > 0) {
-            throw new Error("The following bundled PMD rules are missing from the RULE_MAPPINGS list:\n    " +
+            throw new Error("The following bundled rules are missing from the RULE_MAPPINGS list (Note: when adding them, make sure to add in the appropriate language tags):\n    " +
                 [...missingRuleNamesFromRuleMappings].join(', ') + "\n\n" +
                 "Calculated Rule Descriptions for the missing rules: \n" +
                 JSON.stringify(ruleDescriptions.filter(rd => missingRuleNamesFromRuleMappings.has(rd.name)), null, 2)
