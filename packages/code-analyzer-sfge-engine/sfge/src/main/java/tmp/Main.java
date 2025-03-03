b43fef9f171af73746d3f33ae44a9f6272657d31
Josh Feingold <jfeingold35@gmail.com>
2025-03-03 12:59:29 -0600

    NEW (SFGE) @W-17915640@ Part 2: Added scaffold for Java project (#214)

---
+++ packages/code-analyzer-sfge-engine/sfge/src/main/java/tmp/Main.java
@@ -0,0 +1,11 @@
+package tmp;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().logSomething();
+    }
+
+    private void logSomething() {
+        System.out.println("Printing something");
+    }
+}
